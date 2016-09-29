8b26b7e0c5ed0bbb8fef905f982f6527281c763f
Andrea Zonca <zonca@sdsc.edu>
2016-09-29 11:18:55 -0700

    run interpolation script for micro

---
+++ micro/run_interp.sh
@@ -0,0 +1,14 @@
+#for side in right left
+for side in right
+do
+    #for section in `seq 0 6`
+    for section in `seq 0 0`
+    do
+        #for model in 6um 5um
+        for model in 6um
+        do
+            python interMeMi3m.py $side $section $model
+            python interp2.py
+        done
+    done
+done
