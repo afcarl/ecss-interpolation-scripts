f34d1f329e13224a92b6db59bdc19bac0830cbf0
Andrea Zonca <zonca@sdsc.edu>
2016-09-29 10:52:07 -0700

    more flexible script to run all meso models

--- meso/run_interp.sh
+++ meso/run_interp.sh
@@ -1,5 +1,7 @@
-for i in `seq 0 7`
+for i in `seq 0 6`
 do
-    python interpMaMeO_NewMeso2cei_2747_May13,2016_wCom.py NewMeso_May8,2016.inp NewMeso_May8_2016_right_section_${i}.inp 1 nodecheck_right_femur_$i.inp ../results_macro/rgtd5-SI91pss/U_rotated_back.rpt $i
-    python interpMaMeO_NewMeso2cei_2747_May13,2016_wCom.py NewMeso_May8,2016.inp NewMeso_May8_2016_left_section_${i}.inp 1 nodecheck_left_femur_$i.inp ../results_macro/lgtd4-SI91/U_rotated_back.rpt $i
+    for side in right left
+    do
+        python interpMaMeO_NewMeso2cei_2747_May13,2016_wCom.py NewMeso_May8,2016_nuovo2.inp NewMeso_May8_2016_${side}_section_${i}.inp 1 nodecheck_right_femur_$i.inp ${side} $i
+    done
 done
