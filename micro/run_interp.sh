fdc0849fee41cfa5412bbbe5c23b4102a67abba5
Andrea Zonca <zonca@sdsc.edu>
2016-09-30 12:52:00 -0700

    run production

--- micro/run_interp.sh
+++ micro/run_interp.sh
@@ -1,11 +1,11 @@
-#for side in right left
-for side in right
+for side in right left
+#for side in right
 do
-    #for section in `seq 0 6`
-    for section in `seq 0 0`
+    for section in `seq 0 6`
+    #for section in `seq 0 0`
     do
-        #for model in 6um 5um
-        for model in 6um
+        for model in 6um 5um
+        #for model in 6um
         do
             python interMeMi3m.py $side $section $model
             python interp2.py
