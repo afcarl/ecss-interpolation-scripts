f2b7da3fb5d2cdde6bf0cc43ec69d616fcae7452
Andrea Zonca <zonca@sdsc.edu>
2016-10-24 17:09:00 -0700

    finalized meso

--- meso/run_interp.sh
+++ meso/run_interp.sh
@@ -1,7 +1,7 @@
 for i in `seq 0 6`
 do
-    for side in right left
+    for side in left
     do
-        python interpMaMeO_NewMeso2cei_2747_May13,2016_wCom.py NewMeso_May8,2016_nuovo2.inp NewMeso_May8_2016_${side}_section_${i}.inp 1 nodecheck_right_femur_$i.inp ${side} $i
+        python interpMaMeO_NewMeso2cei_2747_May13,2016_wCom.py NewMeso_May8,2016_nuovo2.inp NewMeso_May8_2016_${side}_section_${i}.inp 1 nodecheck_${side}_femur_$i.inp ${side} $i
     done
 done
--- meso/submit_meso_jobs.py
+++ meso/submit_meso_jobs.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import sys
 import subprocess
+import os.path
 
 if __name__ == "__main__":
 
@@ -12,15 +13,17 @@ if __name__ == "__main__":
         slurm_job_template = f.read()
 
     for input_filename in sys.argv[1:]: # it supports any number of files, you can call it with python submit_meso_jobs.py *.inp
-        #print("Preparing file", input_filename)
 
-        slurm_job_template_filled = slurm_job_template.format(input_filename=input_filename.replace(".inp", ""))
-        print(slurm_job_template_filled)
+        if os.path.exists(input_filename.replace(".inp", ".odb")):
+            print("SKIP", input_filename)
+        else:
+            slurm_job_template_filled = slurm_job_template.format(input_filename=input_filename.replace(".inp", ""))
+            print(slurm_job_template_filled)
 
-        # calls sbatch to submit a job for each input file
-        proc = subprocess.Popen("sbatch", stdin=subprocess.PIPE)
-        try:
-            outs, errs = proc.communicate(slurm_job_template_filled.encode("ascii"), timeout=30)
-        except subprocess.TimeoutExpired:
-            proc.kill()
-            outs, errs = proc.communicate()
+            # calls sbatch to submit a job for each input file
+            proc = subprocess.Popen("sbatch", stdin=subprocess.PIPE)
+            try:
+                outs, errs = proc.communicate(slurm_job_template_filled.encode("ascii"), timeout=30)
+            except subprocess.TimeoutExpired:
+                proc.kill()
+                outs, errs = proc.communicate()
