a8080c33d7d27a78a85603a414a03f8fdc3b7d23
Andrea Zonca <code@andreazonca.com>
2017-07-25 09:39:22 -0700

    Revert "wrong version"
    This reverts commit 7e0e98cd238473b7ab3bbd4f9cee4d3d082329fa.

--- micro/create_S_excel.py
+++ micro/create_S_excel.py
@@ -1,36 +1,27 @@
 import pandas as pd
-import sys
 from ext_set import s
 from glob import glob
 
 for side in ["left", "right"]: # different file
+    writer = pd.ExcelWriter('S_NewMicro_May8_2016_{}.xlsx'.format(side))
     for set_tag in ["ext", "bri"]:
         for model in ["5um", "6um"]: # different sheet
-            #writer = pd.ExcelWriter('S_july17_NewMicro_May8_2016_{}.xlsx'.format("_".join([side, set_tag, model])))
-            output_filename = 'S_july17_NewMicro_May8_2016_{}.csv'.format("_".join([side, set_tag, model]))
             S = []
-            for section in range(0,7):
+            for section in range(7):
                 S_section = []
-                fol = "s/output_july17/outputs_S_july17_NewMicro{model}_May8_2016_{side}_section_{section}/".format(side=side, model=model,section=section)
-                print("****FOLDER:", fol)
-
                 for region in ["CEFM1", "CODS1"]:
-
+                    fol = "s/outputs_S_NewMicro{model}_May8_2016_{side}_section_{section}/".format(side=side, model=model,section=section)
                     filenames = glob(fol + "*{}*{}*.rpt".format(set_tag, region))
-                    for filename in filenames:
-                        print(filename)
-
-                        number_of_nodes = int(filename.split(".")[0].split("_")[-1])
-                        S_section.append(pd.read_csv(filename,
-                                skiprows=22,nrows=number_of_nodes,
-                                delim_whitespace=True, header=None, skip_blank_lines=True, 
-                                skipinitialspace=True,index_col=0,usecols=[0,9,10,11],names=["node","S11-{}".format(section),"S22-{}".format(section),"S33-{}".format(section)])
-                                )
-                        print("section length", len(S_section[-1]))
+                    assert len(filenames) == 1
+                    filename = filenames[0]
+                    print(filename)
+                    number_of_nodes = int(filename.split(".")[0].split("_")[-1])
+                    S_section.append(pd.read_csv(filename,
+                            skiprows=22,nrows=number_of_nodes,
+                            delim_whitespace=True, header=None, skip_blank_lines=True, 
+                            skipinitialspace=True,index_col=0,usecols=[0,9,10,11],names=["node","S11-{}".format(section),"S22-{}".format(section),"S33-{}".format(section)])
+                            )
                 S.append(pd.concat(S_section))
                 
-            tot = S[0].join(S[1:])
-            print("tot", len(tot), len(tot.columns), tot.columns)
-            print("to_excel")
-            tot.to_csv(output_filename)
-            print("Saving")
+            S[0].join(S[1:]).to_excel(writer,set_tag + "_" + model)
+    writer.save()
